6b8ba23b41b315e4e701070a618a061e325361bf
Aleksei Masenko <bestqaengineer7@gmail.com>
2025-07-31 23:09:01 +0300

    дописал несколько тестов. добавил раннер

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -1,19 +1,42 @@
-from unittest import TestCase
+import pytest
+from pytest_mock import mocker
+from calculator.interactive import get_user_input
+from calculator.runner import main
 from unittest.mock import patch
-from calculator import interactive
 
 
-class TestUserInput(TestCase):
-    @patch("builtins.input", side_effect=["5", "+", "3"])
-    def test_valid_input(self, mock_input):
-        num1, op, num2 = interactive.get_user_input()
-        self.assertEqual(num1, 5.0)
-        self.assertEqual(op, "+")
-        self.assertEqual(num2, 3.0)
+@pytest.mark.user_input
+@pytest.mark.parametrize(
+    "mock_inputs, expected",
+    [
+        (["1", "+", "2"], (1.0, "+", 2.0)),
+        (["q", "y"], (None, None, None)),
+        (["q", "no", "3", "*", "4"], (3.0, "*", 4.0)),
+        (["5", "-", "q"], (None, None, None)),
+        (["abc"], (None, None, None)),
+    ]
+)
+def test_get_user_input(mock_inputs, expected, capsys):
+    with patch("builtins.input", side_effect=mock_inputs):
+        result = get_user_input()
+        assert result == expected
 
-    @patch("builtins.input", side_effect=["q", "y"])
-    def test_exit_input(self, mock_input):
-        num1, op, num2 = interactive.get_user_input()
-        self.assertIsNone(num1)
-        self.assertIsNone(op)
-        self.assertIsNone(num2)
+
+@pytest.mark.user_input
+@pytest.mark.parametrize(
+    "mock_return, expected",
+    [
+        ((50, "*", 10), 500),
+        ((50, "/", 10), 5),
+        ((50, "+", 10), 60),
+        ((50, "-", 10), 40),
+        ((-50, "*", 10), -500),
+        ((2, "**", 5), 32)
+
+    ]
+)
+def test_main_with_mocked_input(mocker, mock_return, expected):
+    mocker.patch("calculator.runner.get_user_input", return_value=mock_return)
+
+    result = main()
+    assert result == expected
